ce4fc431e0b7d227bbad21609c6ad6c9bad00cc5
Romain Crestey <rcrestey@gmail.com>
2018-09-18 14:47:02 +0200

    favoris

--- src/vanilla/createLi.js
+++ src/vanilla/createLi.js
@@ -1,8 +1,42 @@
 "use strict";
 
+const favs = new Set(
+  JSON.parse(localStorage.getItem('favs'))
+);
+
 function createLi(tweet) {
   const tweetLi = document.createElement('li');
   tweetLi.textContent = tweet.text;
+
+  const fav = document.createElement('button');
+
+  let isFav = favs.has(tweet.id);
+  fav.textContent = isFav ? 'Supprimer des favs' : 'Ajouter aux favs';
+
+  if (isFav)
+    tweetLi.classList.add('fav');
+
+  fav.addEventListener('click', () => {
+
+    if (isFav) {
+      favs.delete(tweet.id);
+    }
+    else {
+      favs.add(tweet.id);
+    }
+
+    isFav = !isFav;
+    fav.textContent = isFav ? 'Supprimer des favs' : 'Ajouter aux favs';
+
+    tweetLi.classList.toggle('fav');
+
+    const updatedFavs = JSON.stringify(Array.from(favs));
+
+    localStorage.setItem('favs', updatedFavs);
+
+  });
+
+  tweetLi.append(fav);
 
   return tweetLi;
 }
