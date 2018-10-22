c6182bc2b06f1be720139a1bbb504bde60f9ba04
Romain Crestey <rcrestey@gmail.com>
2018-10-22 10:56:49 +0200

    jour 1

---
+++ src/vanilla/main.js
@@ -0,0 +1,78 @@
+import fetchJson from './fetchJson';
+import createOl from './createOl';
+import createTrackingButton from './createTrackingButton';
+
+function isTweetFr({ lang }) {
+  return lang === 'fr';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const urls = [
+    'https://raw.githubusercontent.com/iOiurson/formation/correction/data/tweets.json',
+    'https://raw.githubusercontent.com/iOiurson/formation/correction/data/tweets2.json'
+  ];
+
+  Promise.all(urls.map(fetchJson))
+    .then(([part1, part2]) => {
+      const tweets = part1.concat(part2);
+
+      console.log('Le tableau de tweet', tweets);
+
+      let ol = createOl(tweets);
+
+      const button = document.createElement('button');
+      button.textContent = 'to Fr';
+      document.body.append(button);
+
+      let isFr = false;
+
+      button.addEventListener('click', () => {
+        const tweetsToDisplay = isFr ? tweets : tweets.filter(isTweetFr);
+        trackingButton.textContent = isFr ? 'to Fr' : 'to Int';
+
+        const newOl = createOl(tweetsToDisplay);
+
+        ol.replaceWith(newOl);
+        ol = newOl;
+        isFr = !isFr;
+      });
+
+      const trackingButton = createTrackingButton();
+      document.body.append(trackingButton);
+
+      document.body.append(ol);
+
+
+      // Ajouter les infos de la date à nos <li>
+
+
+      // Ajouter les infos de l'auteur à nos <li>
+
+
+      // Donner des classes à la date ainsi qu'à l'auteur pour les styliser
+
+
+      // Faire un module qui contient une fonction qui crée le bouton de filtre
+
+
+      // ### BONUS 1: LOCALSTORAGE ###
+      // 1) Rajouter un bouton "fav" à chaque li
+
+
+      /* 2) quand le bouton est cliqué, changer le style du li (rajouter une classe 'fav')
+      + et stocker l'ensemble des id_str fav dans le localStorage */
+
+
+      // 3) au chargement de la page, lire le localStorage pour favoriser les favoris.
+
+
+      // ### BONUS 2: TIMING EVENEMENTS ###
+
+      /* Faites un bouton (un peu gros) qui écoute mousedown/click/dblclick et mesure le temps de click et de doubleclick
+      et affiche tempsClic1, tempsClic2, tempsDoubleClic */
+
+
+    })
+    .catch(e => console.error(e));
+
+}, { once: true })
