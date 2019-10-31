a4bb9237901f782a89007ec4ed8030a3bc698910
Romain Crestey <rcrestey@gmail.com>
2019-10-31 17:02:40 +0100

    2019-10-31

--- src/react/Tweet.jsx
+++ src/react/Tweet.jsx
@@ -5,7 +5,7 @@ export default function Tweet(props) {
   return (
     <div className='Tweet'>
       <div>{props.text}</div>
-      <div>{moment(props.created_at).fromNow()}</div>
+      <div>{moment(new Date(props.created_at)).fromNow()}</div>
     </div>
   );
 }
--- src/react/main.jsx
+++ src/react/main.jsx
@@ -1,8 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashLoader } from 'react-spinners';
 
+import fetchJson from '../vanilla/fetchJson';
+import { isTweetFr } from '../utils';
 import TweetList from './TweetList';
 
-import data from '../../data/tweets.json';
+function Filter(props) {
+  return <button onClick={props.filter}>Filtrer</button>;
+}
 
-ReactDOM.render(<TweetList tweets={data} />, document.getElementById('root'));
+const url1 =
+  'https://raw.githubusercontent.com/iOiurson/formation/correction/data/tweets.json';
+const url2 =
+  'https://raw.githubusercontent.com/iOiurson/formation/correction/data/tweets2.json';
+
+class Main extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isFr: false,
+      data: [],
+      authors: [],
+    };
+
+    this.filter = this.filter.bind(this);
+    this.renderContent = this.renderContent.bind(this);
+  }
+
+  filter() {
+    this.setState(state => ({ isFr: !state.isFr }));
+  }
+
+  componentDidMount() {
+    Promise.all([fetchJson(url1), fetchJson(url2)]).then(result => {
+      const authors = [];
+
+      const flatTweets = result.flat();
+
+      flatTweets.forEach(tweet => {
+        if (!authors.includes(tweet.user.name)) {
+          authors.push(tweet.user.name);
+        }
+      });
+
+      this.setState({
+        data: flatTweets,
+        authors,
+      });
+    });
+  }
+
+  renderContent() {
+    const tweetsToDisplay = this.state.isFr
+      ? this.state.data.filter(isTweetFr)
+      : this.state.data;
+
+    console.log('Authors', this.state.authors);
+
+    return (
+      <>
+        <Filter filter={this.filter} />
+        <TweetList tweets={tweetsToDisplay} />
+      </>
+    );
+  }
+
+  render() {
+    const content =
+      this.state.data.length === 0 ? <HashLoader /> : this.renderContent();
+
+    return <div className='Main'>{content}</div>;
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
