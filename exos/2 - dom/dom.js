063f50eaa6820b7f906b05d62513c6c9985dde73
Romain Crestey <rcrestey@gmail.com>
2020-02-16 19:57:09 +0100

    ajout d exos sur le dom

--- exos/2 - dom/dom.js
+++ exos/2 - dom/dom.js
@@ -28,34 +28,57 @@ document.addEventListener(
 
 */
 
-        // ### 1/ Tweets ###
-
         // observer la structure des données des tweets dans la console du navigateur
 
+        // ### BOUCLES ###
+
         console.log('--- Question 1 ---');
+        // afficher dans la console les textes de tous les tweets du tableau en utilisant .forEach()
+
+        console.log('--- Question 2 ---');
+        // créer un tableau avec seulement tous les textes des tweets en utilisant .map()
+
+        console.log('--- Question 3 ---');
+        // créer un tableau avec seulement les dates de publication
+
+        console.log('--- Question 4 ---');
+        // créer un tableau avec seulement les tweets en français en utilisant .filter()
+
+        // ### DOM ###
+
+        console.log('--- Question 5 ---');
         /* pour le premier tweet
           - l'afficher dans la console
           - afficher son texte dans la console
-          - créer une div qui contient le texte du tweet et ajouter la div au <body>
+          - créer une div avec l'id 'replace-me' qui contient le texte du tweet
+          - ajouter la div au <body>
         */
-
-        console.log('--- Question 2 ---');
-        // afficher dans la console les textes de tous les tweets du tableau en utilisant .forEach()
-
-        console.log('--- Question 3 ---');
-        // créer un tableau avec seulement tous les textes des tweets en utilisant .map()
-
-        console.log('--- Question 4 ---');
-        // créer un tableau avec seulement les dates de publication
-
-        console.log('--- Question 5 ---');
-        // créer un tableau avec seulement les tweets en français en utilisant .filter()
 
         console.log('--- Question 6 ---');
         // créer un <ol> et l'ajouter au <body>
 
         console.log('--- Question 7 ---');
         // pour chaque tweet, créer un <li>, mettre le texte dedans, mettre le <li> dans le <ol>
+
+        // !!! S'ARRÊTER LÀ POUR LE MOMENT
+        // ### PRESENTATION DOM ###
+
+        console.log('--- Question 8 ---');
+        // Créer un bouton et remplacer la div 'replace-me' présente dans la page par ce bouton
+
+        // ### EVENEMENTS ###
+
+        console.log('--- Question 9 ---');
+        /* Ajouter un event listener au click sur le bouton précédemment créé
+        pour afficher dans la console le nombre de div présentes dans la page */
+
+        console.log('--- Question 10 ---');
+        /* Ajouter un event listener au scroll sur la window pour modifier la taille de la police
+        en fonction de la hauteur scrollée (event.target.scrollTop) */
+
+        console.log('--- Question 11 ---');
+        /* Créer un bouton qui active le tracking de la position de la souris (event.clientX, event.clientY)
+        et le désactive quand on reclique dessus */
       })
       .catch(function(e) {
         console.error(e);
--- src/vanilla/main.js
+++ src/vanilla/main.js
@@ -12,28 +12,24 @@ document.addEventListener(
       .then(function(tweets) {
         console.log('Le tableau de tweet', tweets);
 
-        // ### 2/ Twitter ###
+        // ### TOUITTER ###
 
         // [1] créer une fonction, qui pour un tweet en entrée, crée et retourne un <li> contenant le texte du tweet
 
         // [2] créer et ajouter un <ol> à la page, puis y ajouter les <li> de tweets en utilisant [1]
 
-        // créer et ajouter un <button> qui quand on clique dessus affiche 'click' dans la console.
+        // [3] modifier la fonction [1] pour ajouter une classe 'fr' ou 'other' au <li> en fonction de la langue
 
-        // [3] modifier le bouton pour que quand on clique dessus, ne garde que les tweets en français
+        // [4] créer et ajouter un <button> qui quand on clique dessus affiche 'click' dans la console.
 
-        // [4] modifier le bouton de filtre pour pouvoir réafficher tous les tweets quand on reclique dessus
+        // [5] modifier le listener du bouton pour que quand on clique dessus, ne garde que les tweets en français
 
-        /* [5] créer une fonction, qui pour un tableau tweets en entrée, crée et retourne un <ol> rempli de <li>
-    et l'utiliser à [2], [3], [4] */
+        // [6] modifier le bouton de filtre pour pouvoir réafficher tous les tweets quand on reclique dessus
 
-        // [6] Créer un bouton qui active le tracking de la position de la souris (event.clientX, event.clientY), et le désactive quand on reclique dessus
+        /* [7] créer une fonction, qui pour un tableau tweets en entrée, crée et retourne un <ol> rempli de <li>
+    et l'utiliser à [2], [5], [6] */
 
-        // PRÉSENTATION Asynchronicité
-
-        // PRÉSENTATION Modules
-
-        // ### BONUS 1: LOCALSTORAGE ###
+        // ### BONUS: LOCALSTORAGE ###
         // [1] Rajouter un bouton "fav" à chaque li
 
         /* [2] quand le bouton est cliqué, changer le style du li (rajouter une classe 'fav')
@@ -41,10 +37,9 @@ document.addEventListener(
 
         // [3] au chargement de la page, lire le localStorage pour favoriser les favoris.
 
-        // ### BONUS 2: TIMING EVENEMENTS ###
+        // PRÉSENTATION Asynchronicité
 
-        /* Faites un bouton (un peu gros) qui écoute mousedown/click/dblclick et mesure le temps de click et de doubleclick
-      et affiche tempsClic1, tempsClic2, tempsDoubleClic */
+        // PRÉSENTATION Modules
       })
       .catch(function(e) {
         console.error(e);
