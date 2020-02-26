9a6a46a009ca06ca62cd280425826e996e44b4af
Romain Crestey <rcrestey@gmail.com>
2020-02-26 15:24:58 +0100

    fix: rename typescript file as .ts and fix format

--- exos/3_typescript/typescript.ts
+++ exos/3_typescript/typescript.ts
@@ -1,31 +1,40 @@
 console.log('--- Question 1 ---');
 // Résoudre les erreurs Typescript pour calculer 3 au lieu de '12'
 function somme(a, b) {
-    return a + b;
+  return a + b;
 }
+
 console.log('Somme', somme(1, '2'));
+
 console.log('--- Question 2 ---');
+
 // Écrire l'interface Person
-var romain = {
-    name: 'Romain',
-    age: '3 4'
+const romain = {
+  name: 'Romain',
+  age: '3 4',
 };
+
 function logPerson(person) {
-    console.log('Name:', person.nom);
-    console.log('Is adult:', person.age > 18);
+  console.log('Name:', person.nom);
+  console.log('Is adult:', person.age > 18);
 }
+
 logPerson(romain);
+
 console.log('--- Question 3 ---');
+
 // Utiliser l'interface Person pour corriger les erreurs TS
-var persons = [
-    romain,
-    { name: 'Michel', age: 65 },
-    { nome: 'Jean-Claude', age: '65' },
+const persons = [
+  romain,
+  { name: 'Michel', age: 65 },
+  { nome: 'Jean-Claude', age: '65' },
 ];
+
 function getAllAges(persons) {
-    var ages = persons.map(function (p) {
-        return p.age;
-    });
-    return ages;
+  const ages = persons.map(function(p) {
+    return p.age;
+  });
+  return ages;
 }
+
 console.log('Ages', getAllAges(persons));
